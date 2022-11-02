5f2f7fddf3d57f4ef155ce835e521459d7cdc72b
matheusinit <matheus.oliveira.s@protonmail.com>
2022-11-02 11:49:32 -0300

    build home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,16 +3,16 @@ import type { FC } from 'react'
 
 export const Header: FC = () => {
   return (
-    <header className="flex w-full justify-center py-6 text-base font-bold">
+    <header className="flex w-full justify-center py-8 text-lg font-medium">
       <nav className="w-52">
         <ul className="flex justify-between text-gray-600">
-          <li className="cursor-pointer hover:text-gray-900">
+          <li className="cursor-pointer text-blue-600 hover:text-blue-600">
             <a href="/">Inicio</a>
           </li>
-          <li className="cursor-pointer hover:text-gray-900">
+          <li className="cursor-pointer hover:text-blue-600">
             <a href="/blog">Blog</a>
           </li>
-          <li className="cursor-pointer hover:text-gray-900">
+          <li className="cursor-pointer hover:text-blue-600">
             <a href="/about">Sobre</a>
           </li>
         </ul>
