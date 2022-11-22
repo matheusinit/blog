d6026c9d6768035f08ee42a125716e1e4e596a9d
matheusinit <matheus.oliveira.s@protonmail.com>
2022-11-22 14:44:51 -0300

    pass posts info to component

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -4,7 +4,7 @@ import reactUuid from 'react-uuid'
 import { format } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 
-interface Params {
+interface Params extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
   title: string
   tags?: string[]
   createdAt?: string
@@ -15,10 +15,11 @@ const defaultCreatedAt = format(new Date(), 'PP', { locale: ptBR })
 export const BlogCard: FC<Params> = ({
   title,
   tags = [],
-  createdAt = defaultCreatedAt
+  createdAt = defaultCreatedAt,
+  ...rest
 }) => {
   return (
-    <div className="flex max-h-40 w-[22rem] flex-col rounded border border-gray-200 px-2 pt-2 pb-4 text-slate-600 shadow-md hover:text-slate-800  md:max-h-48 md:w-96">
+    <div {...rest} className="flex max-h-40 w-[22rem] flex-col rounded border border-gray-200 px-2 pt-2 pb-4 text-slate-600 shadow-md hover:text-slate-800  md:max-h-48 md:w-96" >
       <div className="flex flex-col pb-8">
         <div className="max-h-[4rem] text-xl font-medium">{title}</div>
         <div className="font-light text-slate-500">{createdAt}</div>
