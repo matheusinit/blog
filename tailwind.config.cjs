a83c654b2cb4e92957e9669134ca183fb59b4b95
matheusinit <matheus.oliveira.s@protonmail.com>
2022-11-22 14:02:20 -0300

    lint

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,41 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-	fontFamily: {
-		sans: [
-			"system-ui",
-			"-apple-system",
-			"Roboto",
-			'"Segoe UI"',
-			'"Helvetica Neue"',
-			"Arial",
-			'"Noto Sans"',
-			"sans-serif",
-			'"Apple Color Emoji"',
-			'"Segoe UI Emoji"',
-			'"Segoe UI Symbol"',
-			'"Noto Color Emoji"',
-		],
-		serif: ["ui-serif", "Georgia"],
-		mono: [
-			'"JetBrains Mono"',
-			"monospace",
-			"ui-monospace",
-			"Menlo",
-			"Monaco",
-			'"Cascadia Mono"',
-			'"Segoe UI Mono"',
-			'"Roboto Mono"',
-			'"Oxygen Mono"',
-			'"Ubuntu Monospace"',
-			'"Source Code Pro"',
-			'"Fira Mono"',
-			'"Droid Sans Mono"',
-			'"Courier New"',
-		],
-	},
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [],
+  fontFamily: {
+    sans: [
+      'system-ui',
+      '-apple-system',
+      'Roboto',
+      '"Segoe UI"',
+      '"Helvetica Neue"',
+      'Arial',
+      '"Noto Sans"',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+      '"Noto Color Emoji"'
+    ],
+    serif: ['ui-serif', 'Georgia'],
+    mono: [
+      '"Cascadia Mono"',
+      '"JetBrains Mono"',
+      'monospace',
+      'ui-monospace',
+      'Menlo',
+      'Monaco',
+      '"Segoe UI Mono"',
+      '"Roboto Mono"',
+      '"Oxygen Mono"',
+      '"Ubuntu Monospace"',
+      '"Source Code Pro"',
+      '"Fira Mono"',
+      '"Droid Sans Mono"',
+      '"Courier New"'
+    ]
+  }
 }
