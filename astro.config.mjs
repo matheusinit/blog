bd13614158c7007ef33074dc9da4522e0176c43d
matheusinit <matheus.oliveira.s@protonmail.com>
2022-11-27 14:19:28 -0300

    add syntax highlighting for code snippet

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,20 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), tailwind()]
+  integrations: [mdx(), sitemap(), react(), tailwind()],
+  markdown: {
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'github-dark',
+      wrap: true
+    },
+    drafts: true,
+    extendDefaultPlugins: true
+  }
 });
