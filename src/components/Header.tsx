c8fd4b297f770bd3ccbdea67299cdb03bd4f197f
matheusinit <matheus.oliveira.s@protonmail.com>
2023-03-07 23:14:55 -0300

    chore: fix eslint and lint code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,44 +1,44 @@
-import React, { useEffect, useState } from "react";
-import type { FC } from "react";
-import { SunIcon, MoonIcon } from "@heroicons/react/24/solid/index";
+import React, { useEffect, useState } from 'react'
+import type { FC } from 'react'
+import { SunIcon, MoonIcon } from '@heroicons/react/24/solid/index'
 
 export const Header: FC = () => {
-  const [darkMode, setDarkMode] = useState<boolean>(false);
+  const [darkMode, setDarkMode] = useState<boolean>(false)
 
   useEffect(() => {
     const isOnDarkMode =
-      document.documentElement.classList.contains("dark") ||
-      localStorage.getItem("app-theme") === "dark";
+      document.documentElement.classList.contains('dark') ||
+      localStorage.getItem('app-theme') === 'dark'
 
     if (isOnDarkMode) {
-      document.documentElement.classList.add("dark");
+      document.documentElement.classList.add('dark')
     }
 
-    setDarkMode(isOnDarkMode);
-  }, []);
+    setDarkMode(isOnDarkMode)
+  }, [])
 
   return (
-    <header className="mx-auto flex w-full items-center justify-between py-8 text-base">
-      <a href="/" className="hidden md:flex items-center gap-x-3">
+    <header className="mx-auto mt-8 flex w-full items-center justify-between text-base">
+      <a href="/" className="hidden items-center gap-x-3 md:flex">
         <h1 className="hidden text-xl font-semibold text-white dark:text-gray-200 dark:hover:text-gray-50 md:block">
           matheusinit
         </h1>
       </a>
 
-      <div className="flex justify-center w-full md:w-auto">
+      <div className="flex w-full justify-center md:w-auto">
         {!darkMode && (
           <MoonIcon
             className="h-6 w-6 cursor-pointer text-gray-500 hover:text-gray-800 dark:text-gray-300 dark:hover:text-white"
             onClick={() => {
-              if (document.documentElement.classList.contains("dark")) {
-                document.documentElement.classList.remove("dark");
+              if (document.documentElement.classList.contains('dark')) {
+                document.documentElement.classList.remove('dark')
               } else {
-                document.documentElement.classList.add("dark");
+                document.documentElement.classList.add('dark')
               }
 
-              localStorage.setItem("app-theme", "dark");
+              localStorage.setItem('app-theme', 'dark')
 
-              setDarkMode((previous) => !previous);
+              setDarkMode((previous) => !previous)
             }}
           />
         )}
@@ -47,19 +47,19 @@ export const Header: FC = () => {
           <SunIcon
             className="h-6 w-6 cursor-pointer text-gray-500 hover:text-gray-800 dark:text-gray-300 dark:hover:text-white"
             onClick={() => {
-              if (document.documentElement.classList.contains("dark")) {
-                document.documentElement.classList.remove("dark");
+              if (document.documentElement.classList.contains('dark')) {
+                document.documentElement.classList.remove('dark')
               } else {
-                document.documentElement.classList.add("dark");
+                document.documentElement.classList.add('dark')
               }
 
-              localStorage.setItem("app-theme", "light");
+              localStorage.setItem('app-theme', 'light')
 
-              setDarkMode((previous) => !previous);
+              setDarkMode((previous) => !previous)
             }}
           />
         )}
       </div>
     </header>
-  );
-};
+  )
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,35 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: 'class',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       colors: {
-        "app-violet-dark": "#1a1b26",
-        "app-violet-darker": "#17181f",
-        "app-violet-base": "#24283b",
-        "app-red-base": "#f7768e",
-      },
-    },
+        'app-violet-dark': '#1a1b26',
+        'app-violet-darker': '#17181f',
+        'app-violet-base': '#24283b',
+        'app-red-base': '#f7768e'
+      }
+    }
   },
-  plugins: [require("@tailwindcss/line-clamp")],
+  plugins: [require('@tailwindcss/line-clamp')],
   fontFamily: {
-    sans: ["Comfortaa", "system-ui", "-apple-system", "Roboto"],
-    serif: ["ui-serif", "Georgia"],
+    sans: ['Comfortaa', 'system-ui', '-apple-system', 'Roboto'],
+    serif: ['ui-serif', 'Georgia'],
     mono: [
       '"Roboto Mono"',
       '"JetBrains Mono"',
-      "monospace",
-      "ui-monospace",
-      "Menlo",
-      "Monaco",
+      'monospace',
+      'ui-monospace',
+      'Menlo',
+      'Monaco',
       '"Segoe UI Mono"',
       '"Oxygen Mono"',
       '"Ubuntu Monospace"',
       '"Source Code Pro"',
       '"Fira Mono"',
       '"Droid Sans Mono"',
-      '"Courier New"',
-    ],
-  },
+      '"Courier New"'
+    ]
+  }
 };
