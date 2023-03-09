3f39c0ca57fb76833da50badf551b6e3f0deb800
matheusinit <matheus.oliveira.s@protonmail.com>
2023-03-08 23:10:59 -0300

    feat: add a button go to blog repository

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import type { FC } from 'react'
 import { SunIcon, MoonIcon } from '@heroicons/react/24/solid/index'
+import { IconBrandGithub } from '@tabler/icons'
 
 type AppTheme = 'dark' | 'light'
 
@@ -39,21 +40,29 @@ export const Header: FC = () => {
         </h1>
       </a>
 
-      <button className="flex w-full justify-center rounded-lg bg-app-gray-darker/40 px-3 py-2 hover:bg-app-gray-darker/70 dark:bg-app-violet-base/70 dark:hover:bg-app-violet-base md:w-auto">
-        {!darkMode && (
-          <MoonIcon
-            className="h-6 w-6 cursor-pointer text-gray-500  dark:text-gray-300"
-            onClick={() => toggleThemeTo('dark')}
-          />
-        )}
+      <div className="flex items-center gap-x-4">
+        <a role="button" href="https://github.com/matheusinit/blog" className="flex items-center gap-x-2 rounded-lg bg-app-gray-darker/40 p-3 text-sm hover:bg-app-gray-darker/70 dark:bg-app-violet-base/70 dark:hover:bg-app-violet-darker" target="_blank" rel="noreferrer" >
+          <IconBrandGithub className="h-5 w-5" />
+          <div className="font-bold">View on Github</div>
+        </a>
 
-        {darkMode && (
-          <SunIcon
-            className="h-6 w-6 cursor-pointer text-gray-500 dark:text-gray-300"
-            onClick={() => toggleThemeTo('light')}
-          />
-        )}
-      </button>
+        <button className="flex w-full justify-center rounded-lg bg-app-gray-darker/40 px-3 py-2 hover:bg-app-gray-darker/70 dark:bg-app-violet-base/70 dark:hover:bg-app-violet-darker md:w-auto">
+          {!darkMode && (
+            <MoonIcon
+              className="h-6 w-6 cursor-pointer text-gray-500  dark:text-gray-300"
+              onClick={() => toggleThemeTo('dark')}
+            />
+          )}
+
+          {darkMode && (
+            <SunIcon
+              className="h-6 w-6 cursor-pointer text-gray-500 dark:text-gray-300"
+              onClick={() => toggleThemeTo('light')}
+            />
+          )}
+        </button>
+      </div>
+
     </header>
   )
 }
