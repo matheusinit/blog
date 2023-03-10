406e2c976900a97970b29ad02f6ade7443ca7d7e
matheusinit <matheus.oliveira.s@protonmail.com>
2023-03-10 16:48:46 -0300

    feat: add icon and favicon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -35,18 +35,22 @@ export const Header: FC = () => {
   return (
     <header className="mx-auto mt-8 flex w-full items-center justify-between text-base">
       <a href="/" className="hidden items-center gap-x-3 md:flex">
-        <h1 className="hidden text-xl font-semibold dark:text-gray-200 dark:hover:text-gray-50 md:block">
+        <div className="rounded-full bg-white p-2">
+          <img src="/94.png" alt="" className="h-9 w-9 rounded-full" />
+        </div>
+
+        <h1 className="hidden text-lg font-semibold dark:text-gray-200 dark:hover:text-gray-50 md:block">
           matheusinit
         </h1>
       </a>
 
-      <div className="flex items-center gap-x-4">
+      <div className="flex w-full items-center justify-end gap-x-4 md:w-auto">
         <a role="button" href="https://github.com/matheusinit/blog" className="flex items-center gap-x-2 rounded-lg bg-app-gray-darker/40 p-3 text-sm hover:bg-app-gray-darker/70 dark:bg-app-violet-base/70 dark:hover:bg-app-violet-darker" target="_blank" rel="noreferrer" >
           <IconBrandGithub className="h-5 w-5" />
           <div className="font-bold">View on Github</div>
         </a>
 
-        <button className="flex w-full justify-center rounded-lg bg-app-gray-darker/40 px-3 py-2 hover:bg-app-gray-darker/70 dark:bg-app-violet-base/70 dark:hover:bg-app-violet-darker md:w-auto">
+        <button className="flex w-auto justify-center rounded-lg bg-app-gray-darker/40 px-3 py-2 hover:bg-app-gray-darker/70 dark:bg-app-violet-base/70 dark:hover:bg-app-violet-darker md:w-auto">
           {!darkMode && (
             <MoonIcon
               className="h-6 w-6 cursor-pointer text-gray-500  dark:text-gray-300"
