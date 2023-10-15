0294728e79985e58064860b87798f028cc589717
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-14 22:47:45 -0300

    fix: ensure the date appears correctly

--- src/components/blog/date-formatted.tsx
+++ src/components/blog/date-formatted.tsx
@@ -9,8 +9,13 @@ interface Props {
 }
 
 const DateFormatted: FC<Props> = ({ pubDate }) => {
+  const dateSplitted = pubDate.split('-')
+  const year = Number(dateSplitted[0])
+  const month = Number(dateSplitted[1])
+  const day = Number(dateSplitted[2])
+
   return (
-    <div>{format(new Date(pubDate), 'PP', { locale: ptBR })}</div>
+    <div>{format(new Date(year, month, day), 'PP', { locale: ptBR })}</div>
   )
 }
 
