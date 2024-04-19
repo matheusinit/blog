6d3d54504e9734bf002edbe9efc432d7d0ec4a91
Matheus <matheus.oliveira.s@protonmail.com>
2024-04-19 14:03:48 -0300

    chore: lint code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { IconBrandGithub, IconBrandLinkedin } from '@tabler/icons'
 export const Footer = () => {
   return (
     <div className="mt-12 flex items-center justify-between border-t border-gray-300 pb-4 pt-8 font-['Poppins'] text-gray-800 dark:border-slate-700 dark:text-gray-400">
-      <div className="max-w-[20rem] font-mono text-sm font-semibold lg:max-w-full lg:text-base">
+      <div className="max-w-80 font-mono text-sm font-semibold lg:max-w-full lg:text-base">
         Todas postagens e sistema feito por{' '}
         <a
           href="https://github.com/matheusinit"
