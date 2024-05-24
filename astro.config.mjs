979b2d931a219ae9eefec0591397b4cca73d9534
Matheus <matheus.oliveira.s@protonmail.com>
2024-05-24 15:27:01 -0300

    refactor: remove unused or unintended to use libraries

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import vue from '@astrojs/vue';
 
@@ -12,7 +11,7 @@ export default defineConfig({
   site: 'https://example.com/',
   integrations: [expressiveCode({
     themes: ['github-dark']
-  }), mdx(), sitemap(), react(), tailwind(), vue()],
+  }), mdx(), sitemap(), tailwind(), vue()],
   markdown: {
     drafts: true
   }
--- src/utils/isPostRecent.ts
+++
@@ -1,6 +0,0 @@
-import { isAfter, subDays } from 'date-fns'
-
-export const isPostRecent = (pubDate: string) => {
-  const threeDaysAgo = subDays(new Date(), 3)
-  return isAfter(new Date(pubDate), threeDaysAgo)
-}
