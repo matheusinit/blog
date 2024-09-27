343e00337865bbe6a7108b1a556d13d83dd4279c
matheusinit <matheus.oliveira.s@protonmail.com>
2024-09-27 19:40:02 -0300

    style: redesign home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,18 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import vue from '@astrojs/vue';
-
 import expressiveCode from 'astro-expressive-code';
 
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com/',
   integrations: [expressiveCode({
-    themes: ['github-dark'],
-  }), mdx(), sitemap(), tailwind(), vue()],
+    themes: ['github-dark']
+  }), mdx(), sitemap(), tailwind(), vue(), tailwind({
+    applyBaseStyles: false
+  }), react()],
   markdown: {
     drafts: true
   }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,55 +11,74 @@ const disabledCss = {
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: ['class', 'class'],
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {
-      colors: {
-        'background': 'hsl(var(--background))',
-        'foreground': 'hsl(var(--foreground))',
-
-        'primary': 'hsl(var(--primary))',
-        'primary-foreground': 'hsl(var(--primary-foreground))',
-
-        'secondary': 'hsl(var(--secondary))',
-        'secondary-foreground': 'hsl(var(--secondary-foreground))',
-
-        'foreground-primary': 'hsl(var(--foreground-primary))',
-        'foreground-secondary': 'hsl(var(--foreground-secondary))',
-        'foreground-tertiary': 'hsl(var(--foreground-tertiary))',
-
-        'border': 'hsl(var(--border))',
-        'ring': 'hsl(var(--ring)',
-        'input': 'hsl(var(--input))',
-      },
-      fontFamily: {
-        display: ['Urbanist', 'system-ui', '-apple-system', 'Roboto'],
-        sans: ['PT Sans', 'system-ui', '-apple-system', 'Roboto'],
-        serif: ['ui-serif', 'Georgia'],
-        mono: [
-          'JetBrains Mono',
-          'Roboto Mono',
-          'monospace',
-          'ui-monospace',
-          'Menlo',
-          'Monaco',
-          'Segoe UI Mono',
-          'Oxygen Mono',
-          'Ubuntu Monospace',
-          'Source Code Pro',
-          'Fira Mono',
-          'Droid Sans Mono',
-          'Courier New'
-        ]
-      },
-      typography: {
-        DEFAULT: { css: disabledCss }
-      }
-    }
+  	extend: {
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			'primary-foreground': 'hsl(var(--primary-foreground))',
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			ring: 'hsl(var(--ring))',
+  			input: 'hsl(var(--input))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		fontFamily: {
+  			display: ['system-ui', '-apple-system', 'Urbanist', 'Inter', 'Roboto'],
+  			sans: ['PT Sans', 'system-ui', '-apple-system', 'Roboto'],
+  			serif: ['ui-serif', 'Georgia'],
+  			mono: [ 'JetBrains Mono', 'Roboto Mono', 'monospace', 'ui-monospace', 'Menlo', 'Monaco', 'Segoe UI Mono', 'Oxygen Mono', 'Ubuntu Monospace', 'Source Code Pro', 'Fira Mono', 'Droid Sans Mono', 'Courier New'        ]
+  		},
+  		typography: {
+  			DEFAULT: {
+  				css: 'disabledCss'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
   plugins: [
-    require('@tailwindcss/typography')
-  ],
+    require('@tailwindcss/typography'),
+		require("tailwindcss-animate")
+],
   important: true
 };
